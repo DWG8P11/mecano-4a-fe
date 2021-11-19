6df88cab055b2273bd36d60a0ee06c109e4723c7
Sebastian Puerto <sebastian.cohnan@gmail.com>
2021-11-19 12:18:11 -0500

    Definidas propiedades con texto y letras a practicar en una leccion

--- src/components/CompLeccion.vue
+++ src/components/CompLeccion.vue
@@ -1,7 +1,14 @@
 <template>
     <h1> Componente de Leccion </h1>
 
-    {{ texto }}
+    <p> El texto de la leccion es: <i>{{ texto }}</i></p>
+
+    <p> Las letras que se practicaran en esta leccion es: </p>
+    <ul>
+        <li v-for="letra in letras" :key="letra">
+            {{ letra }}
+        </li>
+    </ul>
 
 
 </template>
@@ -14,6 +21,11 @@ export default {
         texto: {
             type: String,
             default: "No se cargo un texto para esta leccion",
+        },
+
+        letras: {
+            type: Array,
+            default: ['a', 's', 'd', 'f', 'g', 'ñ', 'l', 'k', 'j', 'h']
         }
     }
 }
