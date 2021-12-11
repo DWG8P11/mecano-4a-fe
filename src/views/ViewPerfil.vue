ed263728411863f1eb61192230114d04dd42287d
Sebastian Puerto <sebastian.cohnan@gmail.com>
2021-12-11 17:51:24 -0500

    Se traen correctamente los datos del usuario autenticado

--- src/views/ViewPerfil.vue
+++ src/views/ViewPerfil.vue
@@ -7,8 +7,42 @@
 </template>
 
 <script>
+import gql from "graphql-tag";
+
 export default {
-    name: "ViewPerfil"
+    name: "ViewPerfil",
+
+    created: function() {
+        this.traerDetallesUsuario();
+    },
+
+    methods: {
+        traerDetallesUsuario: async function() {
+            this.$apollo.query(
+                {
+                    query: gql`
+                    query Query {
+                            detallesUsuarioAutenticado {
+                                nombre
+                                usuario
+                                correo
+                                id
+                                telefono
+                                pais
+                                departamento
+                                ciudad
+                                administrador
+                            }
+                    }`
+                }
+            ).then(respuesta => {
+                console.log("Detalles de usuario traidos correctamente.");
+                console.log(JSON.stringify(respuesta));
+            }).catch(error => {
+                console.log("Error trayendo los datos del usuario", error);
+            });
+        }
+    }
 }
 </script>
 
