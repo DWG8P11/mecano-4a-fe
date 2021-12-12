547409953492e8845a590830ce8a98a80c25318e
Sebastian Puerto <sebastian.cohnan@gmail.com>
2021-12-12 02:23:29 -0500

    Estilos basicos Perfil

--- src/views/ViewPerfil.vue
+++ src/views/ViewPerfil.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="view-perfil">
-        <header>
-            <h1> Perfil </h1>
-        </header>
+        <div class="header">
+            Perfil
+        </div>
         <section class="perfil-cuerpo">
             <form id="formulario-usuario-datos">
                 <label for="usuario"> Nombre de Usuario</label>
@@ -29,27 +29,28 @@
                 <label for="administrador"> ¿Es Administrador? </label>
                 <input id="administrador" type="text" v-model="usuarioForm.is_staff" disabled/>
                 <br/>
-
-                <button id="activar-actualizar-datos" class="btn-crud-usuario" type="button" v-if="!actualizandoDatos"
-                        v-on:click="activarModificacionDatos">
-                    Actualizar Datos
-                </button>
-
-                <button id="cancelar-actualizar-datos" class="btn-crud-usuario" type="button" v-if="actualizandoDatos"
-                        v-on:click="cancelarModificacionDatos">
-                    Cancelar Modificación
-                </button>
-
-                <button id="submit-actualizar-datos" class="btn-crud-usuario" type="submit" v-if="actualizandoDatos"
-                        v-on:click.prevent="actualizarDatos">
-                    Actualizar Usuario
-                </button>
-
-                <button id="activar-actualizar-datos" class="btn-crud-usuario" type="button" v-if="actualizandoDatos"
-                        v-on:click="eliminarUsuario">
-                    Eliminar Usuario
-                </button>
                 
+                <div class="contenedor-botones-datos">
+                    <button id="activar-actualizar-datos" class="btn-crud-usuario" type="button" v-if="!actualizandoDatos"
+                            v-on:click="activarModificacionDatos">
+                        Actualizar Datos
+                    </button>
+
+                    <button id="cancelar-actualizar-datos" class="btn-crud-usuario" type="button" v-if="actualizandoDatos"
+                            v-on:click="cancelarModificacionDatos">
+                        Cancelar Modificación
+                    </button>
+
+                    <button id="submit-actualizar-datos" class="btn-crud-usuario" type="submit" v-if="actualizandoDatos"
+                            v-on:click.prevent="actualizarDatos">
+                        Actualizar Usuario
+                    </button>
+
+                    <button id="activar-actualizar-datos" class="btn-crud-usuario" type="button" v-if="actualizandoDatos"
+                            v-on:click="eliminarUsuario">
+                        Eliminar Usuario
+                    </button>
+                </div>
             </form>
         </section>
     </div>
@@ -222,5 +223,102 @@ export default {
 </script>
 
 <style scoped>
+.view-perfil{
+    position: relative;
+    
+    top: -4.5rem;
+    padding-top: 4.5rem;
+    margin: 0;
+    padding: 0;
+    height: calc(100% + 4rem);
+    width: 100%;
 
+    overflow-y: scroll;
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    font-size: 1.3rem;
+    font-family: Questa Grande;
+}
+.header{
+    position: relative;
+    background: yellow;
+    font-size: 2rem;
+    /* top: 0; */
+}
+
+.perfil-cuerpo {
+    /* Centra el contenedor*/
+    position: static;
+    top: 0rem;
+    margin-left: auto;
+    margin-right: auto;
+    height: 50vh;
+
+    /* width: 90vw; */
+    
+    /* display: flex;
+    flex-direction: column; */
+    justify-content: center;
+    align-items: center;
+}
+
+@supports (display: grid) {
+    form {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(24rem, auto));
+    }
+    .view-perfil {
+        display: grid;
+    }
+}
+
+label {
+    /* Necesario para poderle dar un width, porque de lo contrario el label es un inline-element */
+    display: inline-block; 
+}
+
+input{
+    display: inline-block;
+    flex-direction: row;
+    min-width: 20rem;
+    height: 3rem;
+
+    box-sizing: border-box;
+    padding: 10pt 20pt;
+    margin: 0 0;
+
+    border: 1px solid rgb(217,217,217);
+    border-radius: 30px;
+
+}
+
+.contenedor-botones-datos {
+    display: flex;
+    justify-content: center;
+}
+
+button {
+    width: 8rem;
+    height: 3rem;
+
+    color: #E5E7E9;
+    background: rgb(28, 11,127);
+    border: 1px solid #E5E7E9;
+
+    border-radius: 5px;
+    align-content: center;
+
+    font-family: Questa Grande;
+    font-size: 
+}
+
+button:hover{
+    color: #E5E7E9;
+    background: crimson;
+    border: 1px solid rgb(28, 11,127);
+}
 </style>
