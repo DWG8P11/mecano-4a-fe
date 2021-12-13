cc280a58a065e70e5bf406fc09d384bbbbc78d77
Andrea-Quebrada <danseurwaslala@gmail.com>
2021-12-13 14:19:42 -0500

    estilo preliminar para registro de niveles

--- src/views/ViewNivelesCrud.vue
+++ src/views/ViewNivelesCrud.vue
@@ -5,10 +5,10 @@
         <h1>Crear Niveles</h1>
         <section class="formu">
             <form v-on:submit.prevent="GuardarNiveles">
-                <input class="redondeado" type="number" v-model="Niveles.id" placeholder="# Nombre" />
-                <input class="redondeado" type="text" v-model="Niveles.nombre" placeholder="Nombre" />
-                <input class="redondeado" type="text" v-model="Niveles.descripcion" placeholder="Descripcion" />
-                <input type="file" v-on:change="codificarImagenComoURL" />           
+                <input class="redondeado"  type="number" v-model="Niveles.id" placeholder="# Nombre" />
+                <input class="redondeado"  type="text"   v-model="Niveles.nombre" placeholder="Nombre" />
+                <input class="redondeado"  type="text"   v-model="Niveles.descripcion" placeholder="Descripción" />
+                <input class="seleccionar" type="file"   v-on:change="codificarImagenComoURL" />           
                 <!-- Botón para añadir -->
                 
                 <button class="btnGuardar" type="submit">Guardar</button>
@@ -20,16 +20,16 @@
          <center>   <h1>Listado de niveles</h1> </center>
                     <table class="tables" :key="listaNiveles">
                         <thead class="thead-green">
-                            <tr>
+                            <tr class="celdas">
                                 <th scope="col">Id</th>
                                 <th scope="col">Nombres</th>
-                                <th scope="col">Descripcion</th>
+                                <th scope="col">Descripción</th>
                                 <th scope="col">Imagen</th>                         
-                                <th scope="col" style="width: 14%">Action</th>
+                                <th scope="col" style="width: 14%">Acción</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="variable in listaNiveles" v-bind:key="variable.id" v-on:click="metActualizarCampos(variable)">
+                            <tr class="data" v-for="variable in listaNiveles" v-bind:key="variable.id" v-on:click="metActualizarCampos(variable)">
 
                                         <td>{{ variable.id }}</td>
                                         <td>{{ variable.nombre }}</td>
@@ -198,12 +198,24 @@ export default {
 .container h1 {
     text-align: center;
     margin: 0px 0 20px 0;
+    font-family: Questa Grande;
+    color:white;
 }
 
 .container{
-        display: flex;
-        justify-content: center;
-        align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: -8px;
+    padding: 0;
+    background-image: url(../../Imagenes/milkyway.jpg);
+    background-size: cover;
+    background-position: absolute;
+    background-repeat: no-repeat;
+    height: 100vh;
+    overflow: hidden;
+    position: relative;
+    top: -8.5rem;
        
     }
 
@@ -214,7 +226,6 @@ export default {
 .formulario section{
         width: 100%;
         height: 80%;
-
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -223,46 +234,85 @@ export default {
 .container th {
     padding-top: 10px;
     padding-bottom: 10px;
-    text-align: left;
-    background-color: white;
-    color: black;
+    text-align: center;
+    background-color:transparent;
+    color: white;
+    border-radius:10px;
+    border-bottom:1px solid #fcf8f8;
+    box-shadow: 0 0 20px rgb(235, 245, 239)
+   
+
 }
-    .container h1{
-        font-size: 20px;
-        color: Rgb(50,82,136);
-    }
+  
 
 .btnGuardar{
   width:8%;
-  background-color: Rgb(30,174,152);
+  background-color:turquoise;
   color: white;
+  border-radius: 16px;
+  border-color:transparent;
+  font-family: Questa Grande;
+}
+
+.seleccionar{
+   
+    font-family: Questa Grande;
+    color:white;
+
+
 }
 
 .redondeado{
     width: 12vw;
     height: 3vh;
- 
     box-sizing: border-box;
     padding: 10pt 20pt;
     margin: 3pt 0;
- 
     border: 1px solid rgb(217,217,217);
     border-radius: 30px;
+    font-family: Questa Grande;
 }
 
 .btnUpdate{
-    background-color: Rgb(30,174,152);
-    color: white;
+  background:  turquoise;
+  border-radius: 16px;
+  border-radius: 16px;
+  border:none;
+  font-family:Questa Grande;
+  font-size:7.8pt;
+  color:white;
+  box-shadow: 1px 1px 1px 1px rgb(248, 252, 252);
 }
 .btnDelete{
-    background-color: red;
-    color: white;
+  background: turquoise;
+  border-radius: 16px;
+  border-radius: 16px;
+  border:none;
+  font-family:Questa Grande;
+  font-size:7.8pt;
+  color:white;
+  box-shadow: 1px 1px 1px 1px rgb(248, 252, 252);
+  
+}
+.thead-green{
+    font-family: Questa Grande;
+    
+  
 }
 
 .tables{
   padding: 3px 10px;
-  border-radius: 20px;
+  border-bottom:1px solid #fcf8f8;
+  box-shadow: 0 0 10px rgb(235, 245, 239)
 
+  
+  
 }
+
+.data{
+    font-family: Questa Grande;
+    color: white;
+}
+
 
 </style>
